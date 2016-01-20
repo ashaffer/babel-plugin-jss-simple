443c9471e8447fa6607f829daa84af1b5044a2ea
Andrew Shaffer <andrew@weo.io>
2016-01-19 19:47:52 -0800

    update readme & working

--- Readme.md
+++ Readme.md
@@ -7,7 +7,41 @@ Add filename keys to your jss-simple declarations to help with hot reloading
 
 ## Installation
 
-    $ npm install @ashaffer/babel-plugin-jss-simple
+    $ npm install babel-plugin-jss-simple
+
+## What it does
+
+It transforms this:
+
+```javascript
+import css from 'jss-simple'
+
+const style = css({
+  primary: {
+    color: 'green'
+  }
+})
+```
+
+into this:
+
+```javascript
+import css from 'jss-simple'
+
+const style = css({
+  primary: {
+    color: 'green'
+  }
+})
+```
+
+## Why do I want this?
+
+If you are using hot module replacement on the client, you won't be able to tell which styles are the new styles. This provides a key (the filename) to jss-simple so that it can replace the previous style with the new style, and your stylesheet doesn't grow huge.
+
+## Do I have to call it 'css'?
+
+No. It will track whatever you've called your default import and rewrite it appropriately.
 
 ## License
 
--- src/index.js
+++ src/index.js
@@ -3,13 +3,30 @@
  */
 
 function transform ({types: t}) {
+
+  const CallVisitor = {
+    CallExpression (path, {name}) {
+      const node = path.node
+
+      if (node.callee.name === name && node.arguments.length === 1) {
+        path.replaceWith(t.callExpression(t.identifier(name), [
+            node.arguments[0],
+            t.identifier('__filename')
+          ]))
+      }
+    }
+  }
+
   return {
     visitor: {
-      ImportDeclaration (node, parent, scope) {
+      ImportDeclaration (path) {
+        const node = path.node
+        node.parent
         if (node.source.value === 'jss-simple') {
+          let identifier
           node.specifiers.forEach(spec => {
-            if (t.isDefaultImportSpecifier(spec)) {
-              console.log('default import', spec)
+            if (t.isImportDefaultSpecifier(spec)) {
+              path.parentPath.traverse(CallVisitor, {name: spec.local.name})
             }
           })
         }
